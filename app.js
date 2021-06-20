0b942e5f16a9405d76d065202efd338375d123ba
Balaji Dongare <baldonga@in.ibm.com>
2021-06-20 18:53:13 +0530

    feat: user service name

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ var port = 3000;
 app.use("/assets", express.static(__dirname + "/public"));
 //mongo instead of localhost for docker-compose
 mongoose.connect(
-  "mongodb://localhost:27017/users",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  err => {
-    console.log(err);
-  }
+    "mongodb://mongo:27017/users", { useNewUrlParser: true, useUnifiedTopology: true },
+    err => {
+        console.log(err);
+    }
 );
 
 apiController(app);
